e3d618eb7a74c27aaa5a3edc99fbab4da66fc8ba
felipel7 <felipelseven@gmail.com>
2023-10-31 15:07:47 -0300

    chore: setup radix ui
    npm i @radix-ui/themes
    import '@radix-ui/themes/styles.css';

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './Navbar';
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@radix-ui/themes';
+
 export default function Home() {
   return (
     <section>
       <h2>Home page</h2>
+      <Button>Test</Button>
     </section>
   );
 }
