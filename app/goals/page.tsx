b86b92d2febb44e25bf4fd910bff1f0dc5c5480b
felipel7 <felipelseven@gmail.com>
2023-10-31 16:55:34 -0300

    feat(GoalsPage): showing the goals

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -1,9 +1,41 @@
-import { Button } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 
-const GoalsPage = () => {
+const GoalsPage = async () => {
+  const goals = await prisma.goal.findMany();
+
   return (
     <div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Goal</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {goals?.map(goal => (
+            <Table.Row key={goal.id}>
+              <Table.Cell>
+                {goal.title}
+                <p className="block md:hidden">{goal.status}</p>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {goal.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {goal.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
       <Button>
         <Link href="/goals/new">New Goal</Link>
       </Button>
