02146fc4fe2fc4760a436d0671f2998bb5bcbebe
felipel7 <felipelseven@gmail.com>
2023-10-31 13:33:21 -0300

    feat(api): create goals endpoint
    npm i zod
    https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices

---
+++ app/api/goals/route.ts
@@ -0,0 +1,22 @@
+import prisma from '@/prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+import z from 'zod';
+
+const createGoalSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(65535),
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validation = createGoalSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const goal = await prisma.goal.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(goal, { status: 201 });
+}
