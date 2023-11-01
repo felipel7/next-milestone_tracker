fbe5fbf5456f9695c0ff4c86e9717405a1ad1e39
felipel7 <felipelseven@gmail.com>
2023-11-01 11:10:09 -0300

    feat: handle delete error

--- app/goals/[id]/DeleteGoalButton.tsx
+++ app/goals/[id]/DeleteGoalButton.tsx
@@ -4,48 +4,68 @@ import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteGoalButton = ({ goalId }: { goalId: number }) => {
+  const [error, setError] = useState(false);
   const router = useRouter();
 
-  const handleClick = async () => {
+  const deleteGoal = async () => {
     try {
       await axios.delete(`/api/goals/${goalId}`);
       router.push('/goals');
       router.refresh();
     } catch (error) {
-      console.log(error);
+      setError(true);
     }
   };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="ruby">
-          <TrashIcon />
-          Delete Goal
-        </Button>
-      </AlertDialog.Trigger>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="ruby">
+            <TrashIcon />
+            Delete Goal
+          </Button>
+        </AlertDialog.Trigger>
 
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure? This action cannot be undone.
-        </AlertDialog.Description>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure? This action cannot be undone.
+          </AlertDialog.Description>
 
-        <Flex mt="4" gap="3">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
 
-          <AlertDialog.Action onClick={handleClick}>
-            <Button color="ruby">Delete Goal</Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+            <AlertDialog.Action onClick={deleteGoal}>
+              <Button color="ruby">Delete Goal</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This Goal could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            mt="2"
+            variant="soft"
+            onClick={() => setError(false)}
+          >
+            Ok
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
