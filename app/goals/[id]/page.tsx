87d51678bef79dd966374dbca70f2ffade8f3bd9
felipel7 <felipelseven@gmail.com>
2023-11-01 08:39:15 -0300

    feat: add edit button

--- app/goals/[id]/page.tsx
+++ app/goals/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { GoalStatusBadge } from '@/app/components';
 import prisma from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 
@@ -14,18 +16,26 @@ const GoalDetailsPage = async ({ params }: { params: { id: string } }) => {
   if (!goal) notFound();
 
   return (
-    <div>
-      <Heading>{goal.title}</Heading>
-      <Flex gap="2" my="2">
-        <GoalStatusBadge status={goal.status} />
-      </Flex>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Box>
+        <Heading>{goal.title}</Heading>
+        <Flex gap="2" my="2">
+          <GoalStatusBadge status={goal.status} />
+        </Flex>
 
-      <Text>{goal.createdAt.toDateString()}</Text>
+        <Text>{goal.createdAt.toDateString()}</Text>
 
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{goal.description}</ReactMarkdown>
-      </Card>
-    </div>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{goal.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/goals/${goal.id}/edit`}>Editing Goal</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
