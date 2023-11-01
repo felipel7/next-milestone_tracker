1c72fbe551cba9f13b9e30b389d39f01a0e79153
felipel7 <felipelseven@gmail.com>
2023-11-01 10:57:28 -0300

    feat: show a confirmation dialog

--- app/goals/[id]/DeleteGoalButton.tsx
+++ app/goals/[id]/DeleteGoalButton.tsx
@@ -1,12 +1,37 @@
+'use client';
+
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 const DeleteGoalButton = ({ goalId }: { goalId: number }) => {
   return (
-    <Button color="ruby">
-      <TrashIcon />
-      Delete Goal
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="ruby">
+          <TrashIcon />
+          Delete Goal
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure? This action cannot be undone.
+        </AlertDialog.Description>
+
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+
+          <AlertDialog.Action>
+            <Button color="ruby">Delete Goal</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
