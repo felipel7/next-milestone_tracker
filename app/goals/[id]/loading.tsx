8880a83ca19f5a12f97d46f5438793aeb6fa8d62
felipel7 <felipelseven@gmail.com>
2023-11-01 08:24:18 -0300

    style: add aditional loading skeletons

--- app/goals/[id]/loading.tsx
+++ app/goals/[id]/loading.tsx
@@ -1,5 +1,22 @@
-const LoadingDetailsGoalPage = () => {
-  return <div>Loading...</div>;
+import { Box, Flex, Card } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const LoadingGoalDetailsPage = () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex direction="column" gap="2" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </Card>
+    </Box>
+  );
 };
 
-export default LoadingDetailsGoalPage;
+export default LoadingGoalDetailsPage;
