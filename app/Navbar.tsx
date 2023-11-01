0cb872084041da82e7598ea4125fb7d2cd7693ec
felipel7 <felipelseven@gmail.com>
2023-11-01 13:35:31 -0300

    refactor: navbar component

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,16 +13,9 @@ import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { GiTargetShot } from 'react-icons/gi';
-
-const links = [
-  { label: 'Dashboard', href: '/' },
-  { label: 'Goals', href: '/goals/list' },
-];
+import { Skeleton } from './components';
 
 const Navbar = () => {
-  const pathname = usePathname();
-  const { status, data: session } = useSession();
-
   return (
     <nav className="border-b mb-5 px-5 py-4">
       <Container>
@@ -31,50 +24,75 @@ const Navbar = () => {
             <Link href="/">
               <GiTargetShot className="text-blue-500 text-2xl" />
             </Link>
-            <menu className="flex space-x-4">
-              {links.map(link => (
-                <li
-                  key={link.label}
-                  className={classNames({
-                    'text-zinc-900': link.href === pathname,
-                    'text-zinc-500': link.href !== pathname,
-                    'hover:text-zinc-800 transition-colors': true,
-                  })}
-                >
-                  <Link href={link.href}>{link.label}</Link>
-                </li>
-              ))}
-            </menu>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === 'authenticated' ? (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    className="cursor-pointer"
-                    fallback="?"
-                    src={session.user!.image!}
-                    radius="full"
-                    size="2"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content align="end">
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            ) : (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return <Skeleton width="3rem" />;
+
+  if (status === 'unauthenticated')
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            className="cursor-pointer"
+            fallback="?"
+            src={session!.user!.image!}
+            radius="full"
+            size="2"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content align="end">
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Goals', href: '/goals/list' },
+  ];
+
+  return (
+    <menu className="flex space-x-6">
+      {links.map(link => (
+        <li
+          key={link.label}
+          className={classNames({
+            'nav-link': true,
+            '!text-zinc-900': link.href === pathname,
+          })}
+        >
+          <Link href={link.href}>{link.label}</Link>
+        </li>
+      ))}
+    </menu>
   );
 };
 
